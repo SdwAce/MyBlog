b533d8c833ad8a3b3be502a0e0f207dbcd9dfd33
SdwAce <sundiwen163@outlook.com>
2022-01-11 09:42:50 -0800

    finish everything except for theming

--- src/main/backend/com/adobe/blogengine/Config/SecurityConfig.java
+++ src/main/backend/com/adobe/blogengine/Config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.adobe.blogengine.Config;
 import com.adobe.blogengine.Security.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
